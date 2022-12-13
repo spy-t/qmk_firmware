79d21692a1df0e38cebf95254f47fa2d20bbef44
Spyros Trifonidis <mail@spyros.dev>
2022-12-13 11:06:44 +0200

    init kyria miryoku keymap

---
+++ keyboards/splitkb/kyria/keymaps/spyros/rules.mk
@@ -0,0 +1,11 @@
+OLED_ENABLE = false
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = false       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = false      # Enable keyboard RGB underglow
+BOOTLOADER = caterina
+
+MOUSEKEY_ENABLE = yes
+
+CAPS_WORD_ENABLE = yes
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
